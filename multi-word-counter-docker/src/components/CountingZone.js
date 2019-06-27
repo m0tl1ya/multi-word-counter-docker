0d84d8fbed391a7f93564afa41231575b2edf74a
tmoriya <moriya.takayasu514@gmail.com>
2019-06-27 19:47:12 +0900

    change button colors

--- multi-word-counter-docker/src/components/CountingZone.js
+++ multi-word-counter-docker/src/components/CountingZone.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import Refresh from '@material-ui/icons/Refresh';
@@ -15,22 +16,24 @@ const styles = theme => ({
   },
   refreshButton: {
     color: 'white',
-    backgroundColor: '#2196F3',
+    // backgroundColor: '#2196F3',
+    background: 'linear-gradient(45deg, #2196f3 30%, #21cbf3 90%)',
     position: 'fixed',
     bottom: theme.spacing.unit * 12,
     right: theme.spacing.unit * 2,
   },
   addButton: {
     position: 'fixed',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     bottom: theme.spacing.unit * 2,
     right: theme.spacing.unit * 2,
   },
 });
 
 class CountingZone extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     const { classes, counters, actions, mode } = this.props;
@@ -43,22 +46,22 @@ class CountingZone extends Component {
             actions={actions}
             mode={mode}
           />)}
-        <Button
-          variant="fab"
+        <Fab
           color="primary"
+          aria-label="Refresh"
           className={classes.refreshButton}
           onClick={actions.refresh}
         >
           <Refresh />
-        </Button>
-        <Button
-          variant="fab"
-          color="secondary"
+        </Fab>
+        <Fab
+          color="primary"
+          aria-label="Add"
           className={classes.addButton}
           onClick={actions.addCounterBelow}
         >
           <AddIcon />
-        </Button>
+        </Fab>
       </div>
     );
   }
